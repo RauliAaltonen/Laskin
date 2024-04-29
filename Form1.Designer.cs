51c0acbb857efb61ea73e6ba8033777d88115893
RauliAaltonen <97434817+RauliAaltonen@users.noreply.github.com>
2024-04-29 14:37:50 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿
+namespace Laskin1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.yksi = new System.Windows.Forms.Button();
+            this.Laskin = new System.Windows.Forms.Label();
+            this.kaksi = new System.Windows.Forms.Button();
+            this.kolme = new System.Windows.Forms.Button();
+            this.neljä = new System.Windows.Forms.Button();
+            this.viisi = new System.Windows.Forms.Button();
+            this.kuusi = new System.Windows.Forms.Button();
+            this.seitsemän = new System.Windows.Forms.Button();
+            this.kahdeksan = new System.Windows.Forms.Button();
+            this.yhdeksän = new System.Windows.Forms.Button();
+            this.nolla = new System.Windows.Forms.Button();
+            this.desimaali = new System.Windows.Forms.Button();
+            this.yhtäsuuri = new System.Windows.Forms.Button();
+            this.miinus = new System.Windows.Forms.Button();
+            this.plussa = new System.Windows.Forms.Button();
+            this.kerto = new System.Windows.Forms.Button();
+            this.jako = new System.Windows.Forms.Button();
+            this.autoclear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // yksi
+            // 
+            this.yksi.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yksi.Location = new System.Drawing.Point(7, 216);
+            this.yksi.Name = "yksi";
+            this.yksi.Size = new System.Drawing.Size(75, 41);
+            this.yksi.TabIndex = 0;
+            this.yksi.Text = "1";
+            this.yksi.UseVisualStyleBackColor = true;
+            this.yksi.Click += new System.EventHandler(this.yksi_Click);
+            // 
+            // Laskin
+            // 
+            this.Laskin.BackColor = System.Drawing.Color.Black;
+            this.Laskin.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Laskin.ForeColor = System.Drawing.Color.White;
+            this.Laskin.Location = new System.Drawing.Point(8, 10);
+            this.Laskin.Name = "Laskin";
+            this.Laskin.Size = new System.Drawing.Size(317, 67);
+            this.Laskin.TabIndex = 1;
+            this.Laskin.Text = "0";
+            // 
+            // kaksi
+            // 
+            this.kaksi.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kaksi.Location = new System.Drawing.Point(88, 216);
+            this.kaksi.Name = "kaksi";
+            this.kaksi.Size = new System.Drawing.Size(75, 41);
+            this.kaksi.TabIndex = 2;
+            this.kaksi.Text = "2";
+            this.kaksi.UseVisualStyleBackColor = true;
+            this.kaksi.Click += new System.EventHandler(this.kaksi_Click);
+            // 
+            // kolme
+            // 
+            this.kolme.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kolme.Location = new System.Drawing.Point(169, 216);
+            this.kolme.Name = "kolme";
+            this.kolme.Size = new System.Drawing.Size(75, 41);
+            this.kolme.TabIndex = 4;
+            this.kolme.Text = "3";
+            this.kolme.UseVisualStyleBackColor = true;
+            this.kolme.Click += new System.EventHandler(this.kolme_Click);
+            // 
+            // neljä
+            // 
+            this.neljä.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.neljä.Location = new System.Drawing.Point(7, 177);
+            this.neljä.Name = "neljä";
+            this.neljä.Size = new System.Drawing.Size(75, 38);
+            this.neljä.TabIndex = 6;
+            this.neljä.Text = "4";
+            this.neljä.UseVisualStyleBackColor = true;
+            this.neljä.Click += new System.EventHandler(this.neljä_Click);
+            // 
+            // viisi
+            // 
+            this.viisi.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viisi.Location = new System.Drawing.Point(88, 177);
+            this.viisi.Name = "viisi";
+            this.viisi.Size = new System.Drawing.Size(75, 38);
+            this.viisi.TabIndex = 8;
+            this.viisi.Text = "5";
+            this.viisi.UseVisualStyleBackColor = true;
+            this.viisi.Click += new System.EventHandler(this.viisi_Click);
+            // 
+            // kuusi
+            // 
+            this.kuusi.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kuusi.Location = new System.Drawing.Point(169, 177);
+            this.kuusi.Name = "kuusi";
+            this.kuusi.Size = new System.Drawing.Size(75, 38);
+            this.kuusi.TabIndex = 10;
+            this.kuusi.Text = "6";
+            this.kuusi.UseVisualStyleBackColor = true;
+            this.kuusi.Click += new System.EventHandler(this.kuusi_Click);
+            // 
+            // seitsemän
+            // 
+            this.seitsemän.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.seitsemän.Location = new System.Drawing.Point(7, 136);
+            this.seitsemän.Name = "seitsemän";
+            this.seitsemän.Size = new System.Drawing.Size(75, 41);
+            this.seitsemän.TabIndex = 12;
+            this.seitsemän.Text = "7";
+            this.seitsemän.UseVisualStyleBackColor = true;
+            this.seitsemän.Click += new System.EventHandler(this.seitsemän_Click);
+            // 
+            // kahdeksan
+            // 
+            this.kahdeksan.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kahdeksan.Location = new System.Drawing.Point(88, 136);
+            this.kahdeksan.Name = "kahdeksan";
+            this.kahdeksan.Size = new System.Drawing.Size(75, 41);
+            this.kahdeksan.TabIndex = 14;
+            this.kahdeksan.Text = "8";
+            this.kahdeksan.UseVisualStyleBackColor = true;
+            this.kahdeksan.Click += new System.EventHandler(this.kahdeksan_Click);
+            // 
+            // yhdeksän
+            // 
+            this.yhdeksän.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yhdeksän.Location = new System.Drawing.Point(169, 136);
+            this.yhdeksän.Name = "yhdeksän";
+            this.yhdeksän.Size = new System.Drawing.Size(75, 41);
+            this.yhdeksän.TabIndex = 16;
+            this.yhdeksän.Text = "9";
+            this.yhdeksän.UseVisualStyleBackColor = true;
+            this.yhdeksän.Click += new System.EventHandler(this.yhdeksän_Click);
+            // 
+            // nolla
+            // 
+            this.nolla.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nolla.Location = new System.Drawing.Point(88, 258);
+            this.nolla.Name = "nolla";
+            this.nolla.Size = new System.Drawing.Size(76, 37);
+            this.nolla.TabIndex = 18;
+            this.nolla.Text = "0";
+            this.nolla.UseVisualStyleBackColor = true;
+            this.nolla.Click += new System.EventHandler(this.nolla_Click);
+            // 
+            // desimaali
+            // 
+            this.desimaali.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.desimaali.Location = new System.Drawing.Point(7, 258);
+            this.desimaali.Name = "desimaali";
+            this.desimaali.Size = new System.Drawing.Size(75, 37);
+            this.desimaali.TabIndex = 20;
+            this.desimaali.Text = ",";
+            this.desimaali.UseVisualStyleBackColor = true;
+            this.desimaali.Click += new System.EventHandler(this.desimaali_Click);
+            // 
+            // yhtäsuuri
+            // 
+            this.yhtäsuuri.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yhtäsuuri.Location = new System.Drawing.Point(170, 258);
+            this.yhtäsuuri.Name = "yhtäsuuri";
+            this.yhtäsuuri.Size = new System.Drawing.Size(155, 37);
+            this.yhtäsuuri.TabIndex = 22;
+            this.yhtäsuuri.Text = "=";
+            this.yhtäsuuri.UseVisualStyleBackColor = true;
+            this.yhtäsuuri.Click += new System.EventHandler(this.yhtäsuuri_Click);
+            // 
+            // miinus
+            // 
+            this.miinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.miinus.Location = new System.Drawing.Point(250, 216);
+            this.miinus.Name = "miinus";
+            this.miinus.Size = new System.Drawing.Size(75, 41);
+            this.miinus.TabIndex = 24;
+            this.miinus.Text = "-";
+            this.miinus.UseVisualStyleBackColor = true;
+            this.miinus.Click += new System.EventHandler(this.miinus_Click);
+            // 
+            // plussa
+            // 
+            this.plussa.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plussa.Location = new System.Drawing.Point(250, 177);
+            this.plussa.Name = "plussa";
+            this.plussa.Size = new System.Drawing.Size(75, 38);
+            this.plussa.TabIndex = 26;
+            this.plussa.Text = "+";
+            this.plussa.UseVisualStyleBackColor = true;
+            this.plussa.Click += new System.EventHandler(this.plussa_Click);
+            // 
+            // kerto
+            // 
+            this.kerto.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kerto.Location = new System.Drawing.Point(250, 136);
+            this.kerto.Name = "kerto";
+            this.kerto.Size = new System.Drawing.Size(75, 41);
+            this.kerto.TabIndex = 28;
+            this.kerto.Text = "x";
+            this.kerto.UseVisualStyleBackColor = true;
+            this.kerto.Click += new System.EventHandler(this.kerto_Click);
+            // 
+            // jako
+            // 
+            this.jako.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.jako.Location = new System.Drawing.Point(250, 89);
+            this.jako.Name = "jako";
+            this.jako.Size = new System.Drawing.Size(75, 41);
+            this.jako.TabIndex = 30;
+            this.jako.Text = "/";
+            this.jako.UseVisualStyleBackColor = true;
+            this.jako.Click += new System.EventHandler(this.jako_Click);
+            // 
+            // autoclear
+            // 
+            this.autoclear.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.autoclear.Location = new System.Drawing.Point(7, 89);
+            this.autoclear.Name = "autoclear";
+            this.autoclear.Size = new System.Drawing.Size(236, 41);
+            this.autoclear.TabIndex = 32;
+            this.autoclear.Text = "C";
+            this.autoclear.UseVisualStyleBackColor = true;
+            this.autoclear.Click += new System.EventHandler(this.autoclear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 317);
+            this.Controls.Add(this.autoclear);
+            this.Controls.Add(this.jako);
+            this.Controls.Add(this.kerto);
+            this.Controls.Add(this.plussa);
+            this.Controls.Add(this.miinus);
+            this.Controls.Add(this.yhtäsuuri);
+            this.Controls.Add(this.desimaali);
+            this.Controls.Add(this.nolla);
+            this.Controls.Add(this.yhdeksän);
+            this.Controls.Add(this.kahdeksan);
+            this.Controls.Add(this.seitsemän);
+            this.Controls.Add(this.kuusi);
+            this.Controls.Add(this.viisi);
+            this.Controls.Add(this.neljä);
+            this.Controls.Add(this.kolme);
+            this.Controls.Add(this.kaksi);
+            this.Controls.Add(this.Laskin);
+            this.Controls.Add(this.yksi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button yksi;
+        private System.Windows.Forms.Label Laskin;
+        private System.Windows.Forms.Button kaksi;
+        private System.Windows.Forms.Button kolme;
+        private System.Windows.Forms.Button neljä;
+        private System.Windows.Forms.Button viisi;
+        private System.Windows.Forms.Button kuusi;
+        private System.Windows.Forms.Button seitsemän;
+        private System.Windows.Forms.Button kahdeksan;
+        private System.Windows.Forms.Button yhdeksän;
+        private System.Windows.Forms.Button nolla;
+        private System.Windows.Forms.Button desimaali;
+        private System.Windows.Forms.Button yhtäsuuri;
+        private System.Windows.Forms.Button miinus;
+        private System.Windows.Forms.Button plussa;
+        private System.Windows.Forms.Button kerto;
+        private System.Windows.Forms.Button jako;
+        private System.Windows.Forms.Button autoclear;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laskin1
+{
+    public partial class Form1 : Form
+    {
+        string first = "";
+        string second = "";
+        string third = "";
+        char function;
+        double result = 0.0;
+        string userInput = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void yksi_Click(object sender, EventArgs e)
+        {
+            Laskin.Text = "";
+            userInput += "1";
+            Laskin.Text += userInput;
+        }
+
+        private void kaksi_Click(object sender, EventArgs e)
+        {
+            Laskin.Text = "";
+            userInput += "2";
+            Laskin.Text += userInput;
+        }
+
+        private void kolme_Click(object sender, EventArgs e)
+        {
+            Laskin.Text = "";
+            userInput += "3";
+            Laskin.Text += userInput;
+        }
+
+        private void neljä_Click(object sender, EventArgs e)
+        {
+            Laskin.Text = "";
+            userInput += "4";
+            Laskin.Text += userInput;
+        }
+
+        private void viisi_Click(object sender, EventArgs e)
+        {
+            Laskin.Text = "";
+            userInput += "5";
+            Laskin.Text += userInput;
+        }
+
+        private void kuusi_Click(object sender, EventArgs e)
+        {
+            Laskin.Text = "";
+            userInput += "6";
+            Laskin.Text += userInput;
+        }
+
+        private void seitsemän_Click(object sender, EventArgs e)
+        {
+            Laskin.Text = "";
+            userInput += "7";
+            Laskin.Text += userInput;
+        }
+
+        private void kahdeksan_Click(object sender, EventArgs e)
+        {
+            Laskin.Text = "";
+            userInput += "8";
+            Laskin.Text += userInput;
+        }
+
+        private void yhdeksän_Click(object sender, EventArgs e)
+        {
+            Laskin.Text = "";
+            userInput += "9";
+            Laskin.Text += userInput;
+        }
+
+        private void autoclear_Click(object sender, EventArgs e)
+        {
+            function = '\0';
+            first = "";
+            second = "";
+            userInput = "";
+            result = 0.0;
+            Laskin.Text = "0";
+        }
+
+        private void jako_Click(object sender, EventArgs e)
+        {
+            function = '/';
+            first = userInput;
+            userInput = "";
+
+        }
+
+        private void kerto_Click(object sender, EventArgs e)
+        {
+            function = '*';
+            first = userInput;
+            userInput = "";
+
+        }
+
+        private void plussa_Click(object sender, EventArgs e)
+        {
+            function = '+';
+            first = userInput;
+            userInput = "";
+        }
+
+        private void miinus_Click(object sender, EventArgs e)
+        {
+            function = '-';
+            first = userInput;
+            userInput = "";
+
+        }
+
+        private void yhtäsuuri_Click(object sender, EventArgs e)
+        {
+            second = userInput;
+            double firstNum, secondNum;
+            firstNum = Convert.ToDouble(first);
+            secondNum = Convert.ToDouble(second);
+
+            //Plussa
+            if(function == '+')
+            {
+                result = firstNum + secondNum;
+                Laskin.Text = result.ToString();
+            }
+            //Miinus
+            else if(function == '-')
+            {
+                result = firstNum - secondNum;
+                Laskin.Text = result.ToString();
+            }
+            //Jako
+            else if(function == '/')
+            {
+                if(secondNum == '0')
+                {
+                    Laskin.Text = "Error";
+                }
+                else
+                {
+                    result = firstNum / secondNum;
+                    Laskin.Text = result.ToString();
+                }
+            }
+            //Kerto
+            else if(function == '*')
+            {
+                result = firstNum * secondNum;
+                Laskin.Text = result.ToString();
+            }
+        }
+
+        private void nolla_Click(object sender, EventArgs e)
+        {
+            Laskin.Text = "";
+            userInput += "0";
+            Laskin.Text += userInput;
+        }
+
+        private void desimaali_Click(object sender, EventArgs e)
+        {
+            Laskin.Text += ",";
+            userInput += "0";
+            Laskin.Text += userInput;
+        }
+    }
+}
